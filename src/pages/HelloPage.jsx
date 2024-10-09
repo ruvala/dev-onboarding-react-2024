1eedf55993c1d245ad1578f18bd2fceb858fa24d
Gabriel Sessions <gsess7732@gmail.com>
2024-10-09 15:33:24 -0400

    Introduce counter bug

--- src/pages/HelloPage.jsx
+++ src/pages/HelloPage.jsx
@@ -70,7 +70,7 @@ export default function HelloPage() {
         }
         <button
           className="p-3 mx-8 rounded border-green-500 bg-green-500 hover:bg-green-600 text-white"
-          onClick={() => setCount(currentCount => currentCount + 1)}
+          onClick={() => setCount(currentCount => currentCount + 2)}
         >
           Current Count: {count}
         </button>
